e97483daa0f0ae3b87ba332ab7af4e6930674731
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2025-04-04 15:50:16 +0200

    added new ports

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ exports.default = (function (_a) {
     var env = _a.env;
     return ({
         host: env('HOST', '0.0.0.0'),
-        port: env.int('PORT', 1337),
+        port: env.int('PORT', process.env.PORT || 1337),
         app: {
             keys: env.array('APP_KEYS'),
         },
